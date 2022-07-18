a497d5e810fc335e62159c06dfd6a732e8d8e041
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-18 18:08:50 -0400

    quiz1q1

---
+++ quiz1q1.js
@@ -0,0 +1,11 @@
+ 
+// Write a function that counts down from 10 to 1: 
+// (Hint: loops) // paste from your code editor
+
+
+function countDown() {
+    for (let i = 10; i > 0; i--) {
+    console.log(i);
+    }
+    }
+    countDown();
---
+++ quiz1q2.js
@@ -0,0 +1,12 @@
+ 
+// Write a function that adds two numbers (return the result): 
+// paste from your code editor
+
+function twoNumbers(let1, let2) {
+    if(var1 + let2){
+        console.log(var1 + let2)
+    } else console.log(var1 + let2)
+}
+
+// You don't need an if statement and let is preferred to var,
+// please review with your mentor
